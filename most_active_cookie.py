45a259c2fca03d88c50f76863d93a30e3e52d6c5
sinanseslikaya <ss6gw@virginia.edu>
2022-02-11 16:43:20 -0500

    passes tests for empty csv, wrong params, date not provided, date provided, no cookies on date

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -3,6 +3,17 @@ import csv
 import datetime
 
 
+def main():
+    try:
+        if sys.argv[2] == "-d":
+            run_with_date()
+        else:
+            print("Error: command", sys.argv[2], "not defined")
+            exit(1)
+    except IndexError:
+        run()
+
+
 def get_date():
     """
     This function ensures the date command line argument provided is valid
@@ -17,20 +28,61 @@ def get_date():
     return cookie_date
 
 
-def args_provided():
+def args_provided(with_dates):
     """
-    ensures both filename and date args are provided in command line
+    ensures all arguments provided in command line
+    :param with_dates: bool param to decide if func should check for data param or not
     """
+    if with_dates:
+        try:
+            x = len(sys.argv[1]) > 1
+            y = len(sys.argv[3]) > 1
+        except IndexError:
+            print("Error: insufficient arguments")
+            exit(1)
+    else:
+        try:
+            x = len(sys.argv[1]) > 1
+        except IndexError:
+            print("Error: insufficient arguments")
+            exit(1)
+
+
+def run():
+    """
+    finds most active cookie in entire csv
+    """
+
+    args_provided(False)
     try:
-        x = len(sys.argv[1]) > 1
-        y = len(sys.argv[3]) > 1
-    except IndexError:
-        print("Error: insufficient arguments")
+        with open(sys.argv[1]) as file:
+            csv_file = csv.reader(file)
+            cookies = {}
+            for lines in csv_file:
+                if str(lines[0]) not in cookies:
+                    cookies[str(lines[0])] = 1
+                else:
+                    cookies[str(lines[0])] += 1
+            max_occurrences = max(cookies.values())
+
+        for key in cookies.items():
+            if key[1] == max_occurrences:
+                print(key[0])
+    except ValueError:
+        # error arises from max() having an empty arg -> no cookies
+        print("Error: no cookies")
+        exit(1)
+    except FileNotFoundError:
+        print("Error: file not found")
         exit(1)
 
 
-def main():
-    args_provided()
+def run_with_date():
+    """
+    finds most active cookie on provided date
+    """
+
+    args_provided(True)
     date = get_date()
     try:
         with open(sys.argv[1]) as file:
@@ -55,5 +107,6 @@ def main():
     except FileNotFoundError:
         print("Error: file not found")
         exit(1)
+
 
 main()
