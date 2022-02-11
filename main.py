b370b702dfe6630dd5dfbe0cdbb20ca0c225de54
Sinan Seslikaya <ss6gw@virginia.edu>
2022-02-11 15:35:32 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -19,24 +19,33 @@ def dateCheck():
     return correctDate
 
 
-if len(sys.argv[1]) > 1 and dateCheck() == True:
-    # filepath = sys.argv[1]
-    try:
-        with open(sys.argv[1]) as file:
-            csvFile = csv.reader(file)
-            cookies = {}
+def run():
 
-            for lines in csvFile:
-                if lines[1][0:10] == sys.argv[3]:  # checks if correct date
-                    if str(lines[0]) not in cookies:
-                        cookies[str(lines[0])] = 1
-                    else:
-                        cookies[str(lines[0])] += 1
-            print(max(cookies, key =cookies.get))
+    if len(sys.argv[1]) > 1 and dateCheck() == True:
+        # filepath = sys.argv[1]
+        try:
+            with open(sys.argv[1]) as file:
+                csvFile = csv.reader(file)
+                cookies = {}
 
-    except FileNotFoundError:
-        print("File not found")
+                for lines in csvFile:
+                    if lines[1][0:10] == sys.argv[3]:  # checks if correct date
+                        if str(lines[0]) not in cookies:
+                            cookies[str(lines[0])] = 1
+                        else:
+                            cookies[str(lines[0])] += 1
+
+                max_keys = [key for key, value in cookies.items() if value == max(cookies.values())]
+
+                for key in max_keys:
+                    print(key)
+
+        except FileNotFoundError:
+            print("File not found")
+            exit(1)
+    else:
+        print('You need file path')
         exit(1)
-else:
-    print('You need file path')
-    exit(1)
+
+#TODO CHECK runtime conditinos
+run()
